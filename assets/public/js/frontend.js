1ce4ad76718d2d2e7f48d4aba0c79bf41f6fa6cd
zeein <zeein81@gmail.com>
2021-12-27 21:46:21 +0900

    지도에서 마커 제외

--- assets/public/js/frontend.js
+++ assets/public/js/frontend.js
@@ -35251,6 +35251,62 @@ var initAboutUs = function initAboutUs() {
   Array.from(conponents).forEach(function (el, index) {
     new initSwiper(el, index);
   });
+  var mapContainer = document.getElementById('map');
+  var _window = window,
+      kakao = _window.kakao;
+
+  if (mapContainer) {
+    var mapOption = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      // 지도의 중심좌표
+      level: 4 // 지도의 확대 레벨
+
+    }; // 지도를 생성합니다
+
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 줌 막기
+
+    map.setZoomable(false); // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+
+    var mapTypeControl = new kakao.maps.MapTypeControl(); // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT); // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT); // 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+
+    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC); // 주소-좌표 변환 객체를 생성합니다
+
+    var geocoder = new kakao.maps.services.Geocoder();
+    var marker, content, customOverlay; // 주소로 좌표를 검색합니다
+
+    geocoder.addressSearch('서울 마포구 월드컵북로8길 21 인클루드빌딩 3층', function (result, status) {
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x); // 결과값으로 받은 위치를 마커로 표시합니다
+        // marker = new kakao.maps.Marker({
+        // 	map,
+        // 	position: coords,
+        // });
+
+        content = '<div class ="bg-white text-xs py-2 px-5 rounded-sm bg-opacity-90"><span class="left"></span><span class="center">인클루드시스</span><span class="right"></span></div>'; // 커스텀 오버레이를 생성합니다
+
+        customOverlay = new kakao.maps.CustomOverlay({
+          content: content,
+          position: coords
+        }); // 커스텀 오버레이를 지도에 표시합니다
+
+        customOverlay.setMap(map); // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+
+        map.setCenter(coords);
+      }
+    });
+    window.addEventListener('resize', function () {
+      var markerPosition = customOverlay.getPosition();
+      map.relayout();
+      map.setCenter(markerPosition);
+    }, true);
+  }
 };
 
 /* harmony default export */ const pageAbout = (initAboutUs);
